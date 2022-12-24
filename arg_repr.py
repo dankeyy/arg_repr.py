11c6727a84b137d4a74caa7ae4c0f6bff437b81b
dankeyy <dankeyy@protonmail.com>
2022-12-24 17:05:11 +0200

    added support for interactive mode

--- arg_repr.py
+++ arg_repr.py
@@ -3,13 +3,43 @@ import sys
 import inspect
 
 
+def _prompt(func_name):
+    # awfully hacky, but pretty flexible for prompt lol
+    import re, readline
+    i = readline.get_current_history_length()
+    line = ''
+    code = []
+
+    while not re.match(r".*{}\s*\(".format(func_name), line):
+        line = readline.get_history_item(i)
+        relevant = line.lstrip("\\\n")
+
+        # tbh at this point just exploring how many edge parsing cases
+        # i could possibly catch without a tokenizer
+        if relevant:
+            if relevant == func_name and code[-1] == '(':
+                return ''.join(code[:-1:-1])
+
+            code.append(relevant)
+        i -= 1
+    return ''.join(code[::-1])
+
+
+def _in_ipython():
+    try:
+        return __IPYTHON__
+    except NameError:
+        return False
+
+
 def _parens(code):
+    """Get index of closing paren"""
     opening = closing = 0
     marks = []
 
     for i, v in enumerate(code):
 
-        if code[i-1] != '\\' or code[i-2:i] == "\\\\":
+        if code[i-1] != "\\" or code[i-2:i] == "\\\\":
 
             if v in ("'", '"'):
                 marks.append(v)
@@ -28,25 +58,32 @@ def _parens(code):
             return i
 
 
-def myargs_repr():
+def arg_repr():
     func_name = sys._getframe(1).f_code.co_name
-    upper_frame = sys._getframe(2)
+    in_ipython = _in_ipython()
+    in_interactive_shell = hasattr(sys, "ps1")
 
-    #        line called              first line in frame that called
-    lineo = upper_frame.f_lineno - upper_frame.f_code.co_firstlineno
-    code = inspect.getsource(upper_frame)
+    if in_interactive_shell and not in_ipython:
+        code = _prompt(func_name)
 
-    # seek beginning of call line by traversing the code until the lineo-th newline
-    line = p = 0
-    while line != lineo:
-        if code[p] == os.linesep:
-            line += 1
-        p += 1
+    else: # in ipython/ file
+        upper_frame = sys._getframe(2)
+        #        line called              first line in frame that called
+        lineo = upper_frame.f_lineno - upper_frame.f_code.co_firstlineno
+        code = inspect.getsource(upper_frame)
 
-    # slice the code
-    # from beginning of call (after opening paren)
-    # up until the last relevant closing paren
-    code = code[p:]
+        # seek beginning of call line by traversing the code until the lineo-th newline
+        line = p = 0
+        while line != lineo:
+            if code[p] == os.linesep:
+                line += 1
+            p += 1
+
+        # slice the code
+        # from beginning of call (after opening paren)
+        # up until the last relevant closing paren
+        code = code[p:]
+
     code = code[code.index(func_name) + len(func_name) + 1:]
     code = code[:_parens(code)] # _parens might return None but that's ok, [:None] is valid
 
